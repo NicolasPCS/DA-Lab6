924e36abbb38008103a0f148d2a08ce1845794a0
Nicolás <nicolascaytuirosilva@gmail.com>
2021-09-29 14:20:26 -0500

    Se añadio la implementacion de la aplicacion para que se muestren datos de la tabla productos, realizada en la exp. de practicas 1

---
+++ Basededatos/Models/ProductosForm.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Basededatos.Models
+{
+    public partial class ProductosForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source = DESKTOP-KKA5IBN\\SQLEXPRESS;" + "Initial Catalog = VENTAS; Integrated Security = True";
+            string selectSQL = "SELECT * FROM PRODUCTOS";
+            SqlConnection conexion = new SqlConnection(connectionString);
+            SqlCommand comando = new SqlCommand(selectSQL, conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+
+            // Llenado del Dataset
+            DataSet ventas = new DataSet();
+            adapter.Fill(ventas, "productos");
+
+            // Enlazar el GridView
+            GridView1.DataSource = ventas;
+            GridView1.DataBind();
+        }
+    }
+}
